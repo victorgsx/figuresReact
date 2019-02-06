18f05d9e667bd3206db9bbd618948be662b436e0
Victor <victor.fwm@gmail.com>
2019-02-06 13:06:30 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Row, Col, Container } from "react-bootstrap";
+
+import FigureForm from "./Components/FigureForm";
+import FigureList from "./Components/FigureList";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+
+class App extends Component {
+
+	typesConfig = [
+		{
+			id: "circle",
+			title: "Круг",
+			fields: [
+				{
+					id: "radius",
+					title: "Радиус",
+				}
+			],
+			getArea: (params) => Math.PI * Math.pow(params.radius, 2)
+		},
+		{
+			id: "square",
+			title: "Квадрат",
+			fields: [
+				{
+					id: "sideA",
+					title: "Сторона А",
+				}
+			],
+			getArea: (params) => Math.pow(params.sideA, 2)
+		},
+	];
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			figures: [],
+		};
+
+		this.addFigure = this.addFigure.bind(this);
+	}
+
+	addFigure(figure) {
+		this.setState({figures: [...this.state.figures, figure]});
+	}
+
+	render() {
+		return (
+			<Container>
+				<Row className="mt-4">
+					<Col>
+						<FigureForm typesConfig={this.typesConfig} handleSubmit={this.addFigure}/>
+					</Col>
+					<Col md={8}>
+						<FigureList figures={this.state.figures}/> 
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
+};
+
+export default App;
---
+++ src/Components/FigureList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+
+class FigureList extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		};
+		
+	}
+
+	render() {
+		const {figures} = this.props;
+
+		return (
+			<Table striped bordered hover>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>TypeID</th>
+						<th>Area</th>
+					</tr>
+				</thead>
+				<tbody>
+					{figures.map((figureItem) => {
+						return (
+							<tr key={figureItem.id}>
+								<td>{figureItem.id}</td>
+								<td>{figureItem.typeId}</td>
+								<td>{figureItem.area}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</Table>
+        );
+	}
+};
+
+export default FigureList;
